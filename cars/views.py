3d7819007e4a118ad6b04ff623dee39223a9e6ed
KirillDegtyarev <lirik19@yandex.ru>
2023-05-20 00:05:16 +0700

    new

--- cars/views.py
+++ cars/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Car
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 
 def cars(request):
     cars = Car.objects.order_by("-created_date")
+    paginator = Paginator(cars, 4)
+    page = request.GET.get('page')
+    paged_cars = paginator.get_page(page)
 
     data = {
-        'cars': cars,
+        'cars': paged_cars,
     }
     return render(request, 'cars/cars.html', data)
 
@@ -18,3 +22,43 @@ def car_detail(request, id):
         'single_car': single_car,
     }
     return render(request, 'cars/car_detail.html', data)
+
+
+def search(request):
+    cars = Car.objects.order_by('-created_date')
+
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            cars = cars.filter(description_icontains=keyword)
+
+    if 'model' in request.GET:
+        model = request.GET['model']
+        if model:
+            cars = cars.filter(model__iexact=model)
+
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            cars = cars.filter(model__iexact=city)
+
+    if 'year' in request.GET:
+        year = request.GET['year']
+        if year:
+            cars = cars.filter(model__iexact=year)
+
+    if 'body_style' in request.GET:
+        body_style = request.GET['body_style']
+        if body_style:
+            cars = cars.filter(model__iexact=body_style)
+
+    if 'min_price' in request.GET:
+        min_price = request.GET('min_price')
+        max_price = request.GET('max_price')
+        if max_price:
+            cars = cars.filter(price__gte=min_price, price__lte=)
+    data = {
+        'cars': cars,
+    }
+
+    return render(request, 'cars/search.html', data)
